5b63963de0548c9c49cbdfbe1236b052cb783386
a1878697 <hoangharry2309@MSI.localdomain>
2025-12-21 13:31:58 +1030

    Added .env example file to .gitignore; Added simple PDF parser function; Added simple gemini-client call function (unfinished)

---
+++ source/pdfParser.py
@@ -0,0 +1,10 @@
+import pdfplumber as pp
+
+# Function to parse the PDF file or path to a PDF file
+# Accepts 1 page pdf only
+def parse_pdf(pdf_path):
+    with pp.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text = page.extract_text()
+            print(text)
+            return text
