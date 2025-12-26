6e9027229d00e758696757e6385ea7aba060cacf
DangKhoaNguyen <dangkhoa04052005@gmail.com>
2025-12-26 20:35:15 +1030

    (Addition): Add the function to get the JSON object from ai text.

--- source/api/transactions.py
+++ source/api/transactions.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify
 from db.category_extractor import get_transaction_categories
 from ai.gemini_client import extract_transactions
 from flows.pdf_parser import parse_pdf
+from flows.ai_parser import parse_ai_json
 import json
 
 bp = Blueprint("transactions", __name__, url_prefix="/transactions")
@@ -15,22 +16,15 @@ def get_transactions():
 
 @bp.get("/detailed_transactions")
 def get_detailed_transactions():
-    pdf_text = parse_pdf("payslip/PaySlip.pdf")
+    pdf_text = parse_pdf("payslip/your_pay_slip")
 
     if not pdf_text.strip():
         return {"error": "No text found in PDF"}, 400
 
-    # Testing
-    result = '''
-        {
-        "transaction_date": "2025-12-15",
-        "transaction_details": "Salary Payment",
-        "transaction_amount": "$402.50",
-        "transaction_category": "Other",
-        "transaction_type": 2,
-        "relevant": true
-        }
-        '''
+    # Extract the JSON form from ai
+    result = extract_transactions(pdf_text)
+    
+    # Take the JSON object.
+    parse_ai = parse_ai_json(result)
 
-    print(json.loads(result))
-    return [json.loads(result)]
+    return [parse_ai]
