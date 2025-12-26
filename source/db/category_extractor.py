b1a860f5f64c1b2326dbcc96450beeae70ade0ea
a1878697 <huynhphihoang2309@gmail.com>
2025-12-27 00:41:10 +1030

    Changed function input keyword

--- source/db/category_extractor.py
+++ source/db/category_extractor.py
@@ -1,5 +1,6 @@
-def get_transaction_categories_with_ids() -> list[dict]:
-    response = supabase.table("Category List") \
+# This function should be used to get trnsaction categories FROM SUPABASE 
+def get_transaction_categories_with_ids(SUPABASE_CLIENT) -> list[dict]:
+    response = SUPABASE_CLIENT.table("Category List") \
         .select("category_id, category_name") \
         .execute()
 
